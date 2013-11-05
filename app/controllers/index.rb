1e7174288f40641de108b54b64e089b12d1170ab
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-04 21:02:52 -0800

    made it so the forms are prefilled when users update their post

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,13 +6,9 @@ end
 get '/categories/:id' do
   @category_id = params[:id]
   @category_title = Category.find(@category_id).title
+  p @category_title
+  p @category_id
   @postings = Posting.where(category_id: @category_id)
-
-  if post_exists?
-    @last_post = 1
-  else
-    @last_post = Posting.all.last.id + 1
-  end
 
   erb :postings
 end
@@ -21,10 +17,11 @@ get '/postings/:category_id/:id' do
   @category_id = params[:category_id]
   @post = Posting.find(params[:id])
   @category_title = Category.find(@category_id).title
+
   erb :single_post
 end
 
-post '/postings/create/new/:category/:post' do
+post '/postings/create/new/:category' do
   @post_title = params[:posting][:title]
   @post_body = params[:posting][:body]
   @category_id = params[:category]
@@ -34,25 +31,15 @@ post '/postings/create/new/:category/:post' do
 
   post = Posting.create(title: @post_title, body: @post_body, category_id: params[:category], price: @post_price, location: @post_location)
 
-  @post_id = post[:id]
+  @post_id = post.id
 
-  # erb :new_post
   redirect "/postings/#{post.slug}"
 end
 
-# get '/postings/create/new/:category/:post' do
 get '/postings/:post_slug' do 
   @post_id = Posting.find_by_slug(params[:post_slug]).id
-  post = Posting.find(@post_id)
-
-  @post_title = post.title
-  @post_body = post.body
-  @post_price = post.price
-  @post_location = post.location
-  # @post_id = params[:post]
-
-  # @category_id = params[:category]
-  @category_id = post.category_id
+  @post = Posting.find(@post_id)
+  @category_id = @post.category_id
   @category_title = Category.find(@category_id).title
 
   erb :new_post
@@ -65,10 +52,11 @@ end
 
 put '/update/:category_id/:post_id' do
   @category_id = params[:category_id]
+  @category_title = Category.find(@category_id).title
+
   @post_id = params[:post_id]
   @post = Posting.find(@post_id)
   @post.update_attributes title: params[:title], body: params[:body], category_id: @category_id, location: params[:location], price: params[:price]
-  
-  @category_title = Category.find(@category_id).title
+
   erb :single_post
 end
